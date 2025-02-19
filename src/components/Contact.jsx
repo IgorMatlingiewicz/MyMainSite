40cb3cea8c6d382fb7f057c3f94850ddea2069ad
Igor Matlingiewicz <igor.matlingiewicz@gmail.com>
2025-02-19 17:04:22 +0100

    Started working on form.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,16 +15,16 @@ export default function Contact({ language }) {
             <h4 className='fw-bold'><i class="bi bi-envelope"></i> igor.matlingiewicz@gmail.com</h4>
             <h4 className='fw-bold'><i class="bi bi-telephone"></i> 697-327-580</h4>
             <h3><i class="bi bi-chat-left-dots"></i> {content.contact.form['formInfo']}</h3>
-            <Form>
+            <Form name="contact" netlify>
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridEmail" md={4} xs={12}>
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Email" />
+                        <Form.Control type="email" placeholder="Email" required/>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword" md={4} xs={12}>
                         <Form.Label>{content.contact.form["name"]}</Form.Label>
-                        <Form.Control type="text" placeholder={content.contact.form["name"]} />
+                        <Form.Control type="text" placeholder={content.contact.form["name"]} required/>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword" md={4} xs={12}>
@@ -35,11 +35,11 @@ export default function Contact({ language }) {
 
                 <Form.Group className="mb-3" controlId="formGridAddress1">
                     <Form.Label>{content.contact.form["message"]}</Form.Label>
-                    <Form.Control as="textarea" placeholder={content.contact.form["message"]} rows={4} />
+                    <Form.Control as="textarea" placeholder={content.contact.form["message"]} rows={4} required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" id="formGridCheckbox">
-                    <Form.Check type="checkbox" label={content.contact.form["agreement"]}/>
+                    <Form.Check type="checkbox" label={content.contact.form["agreement"]} required/>
                 </Form.Group>
 
                 <Button variant="warning" type="submit" className='fw-bold'>
