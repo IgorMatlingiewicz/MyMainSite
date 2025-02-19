3d46333727922e552559aa0b53819adebf71a6d1
Igor Matlingiewicz <igor.matlingiewicz@gmail.com>
2025-02-19 19:26:27 +0100

    Improved netlify forms options.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,38 +9,57 @@ import contentEN from "../data/contentEN.json";
 export default function Contact({ language }) {
     let content = language == "PL" ? contentPL : contentEN;
 
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Zatrzymanie domyślnej akcji formularza
+
+        const form = e.target;
+        const data = new FormData(form);
+
+        fetch("/", {
+            method: "POST",
+            body: data,
+        })
+            .then(() => {
+                alert("Formularz został wysłany!");
+                form.reset(); // Opcjonalne czyszczenie formularza
+            })
+            .catch((error) => {
+                alert("Błąd wysyłania formularza: " + error);
+            });
+    };
+
     return (
         <>
             <h3>{content.contact["contactOptions"]}</h3>
             <h4 className='fw-bold'><i class="bi bi-envelope"></i> igor.matlingiewicz@gmail.com</h4>
             <h4 className='fw-bold'><i class="bi bi-telephone"></i> 697-327-580</h4>
             <h3><i class="bi bi-chat-left-dots"></i> {content.contact.form['formInfo']}</h3>
-            
-            <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+
+            <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridEmail" md={4} xs={12}>
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Email" required name='email'/>
+                        <Form.Control type="email" placeholder="Email" required name='email' />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword" md={4} xs={12}>
                         <Form.Label>{content.contact.form["name"]}</Form.Label>
-                        <Form.Control type="text" placeholder={content.contact.form["name"]} required name='name'/>
+                        <Form.Control type="text" placeholder={content.contact.form["name"]} required name='name' />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword" md={4} xs={12}>
                         <Form.Label>{content.contact.form["phone"]}</Form.Label>
-                        <Form.Control type="text" placeholder={content.contact.form["phone"]} name='phone'/>
+                        <Form.Control type="text" placeholder={content.contact.form["phone"]} name='phone' />
                     </Form.Group>
                 </Row>
 
                 <Form.Group className="mb-3" controlId="formGridAddress1">
                     <Form.Label>{content.contact.form["message"]}</Form.Label>
-                    <Form.Control as="textarea" placeholder={content.contact.form["message"]} rows={4} required name='message'/>
+                    <Form.Control as="textarea" placeholder={content.contact.form["message"]} rows={4} required name='message' />
                 </Form.Group>
 
                 <Form.Group className="mb-3" id="formGridCheckbox">
-                    <Form.Check type="checkbox" label={content.contact.form["agreement"]} required/>
+                    <Form.Check type="checkbox" label={content.contact.form["agreement"]} required />
                 </Form.Group>
 
                 <Button variant="warning" type="submit" className='fw-bold'>
