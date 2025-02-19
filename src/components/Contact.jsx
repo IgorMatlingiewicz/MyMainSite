57636eb92f528d411bd32a963b2346d15d5dca36
Igor Matlingiewicz <igor.matlingiewicz@gmail.com>
2025-02-19 17:15:00 +0100

    Added names for inputs in form.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,27 +15,27 @@ export default function Contact({ language }) {
             <h4 className='fw-bold'><i class="bi bi-envelope"></i> igor.matlingiewicz@gmail.com</h4>
             <h4 className='fw-bold'><i class="bi bi-telephone"></i> 697-327-580</h4>
             <h3><i class="bi bi-chat-left-dots"></i> {content.contact.form['formInfo']}</h3>
-            <Form name="contact" netlify>
+            <Form name="contact" method="POST" data-netlify="true" onSubmit="submit">
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridEmail" md={4} xs={12}>
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Email" required/>
+                        <Form.Control type="email" placeholder="Email" required name='email'/>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword" md={4} xs={12}>
                         <Form.Label>{content.contact.form["name"]}</Form.Label>
-                        <Form.Control type="text" placeholder={content.contact.form["name"]} required/>
+                        <Form.Control type="text" placeholder={content.contact.form["name"]} required name='name'/>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword" md={4} xs={12}>
                         <Form.Label>{content.contact.form["phone"]}</Form.Label>
-                        <Form.Control type="text" placeholder={content.contact.form["phone"]} />
+                        <Form.Control type="text" placeholder={content.contact.form["phone"]} name='phone'/>
                     </Form.Group>
                 </Row>
 
                 <Form.Group className="mb-3" controlId="formGridAddress1">
                     <Form.Label>{content.contact.form["message"]}</Form.Label>
-                    <Form.Control as="textarea" placeholder={content.contact.form["message"]} rows={4} required/>
+                    <Form.Control as="textarea" placeholder={content.contact.form["message"]} rows={4} required name='message'/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" id="formGridCheckbox">
